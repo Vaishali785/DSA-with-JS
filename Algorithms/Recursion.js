2381e22a953b882a6bdafd07a9195d6958675477
Vaishali <aggarwal.vaishali785@gmail.com>
2024-02-05 17:31:24 +0530

    docs: added a little function of recursion

---
+++ Algorithms/Recursion.js
@@ -0,0 +1,16 @@
+let counter = 0;
+function inception() {
+    if (counter > 3) {
+        return "done";
+    }
+    counter++;
+    return inception();
+}
+
+inception();
+
+
+
+// 1. Identify the base case (or stop point). 
+// 2. Identify the recursive case.
+// 3. Get closer and closer and return when needed. Usually you have 2 returns.
