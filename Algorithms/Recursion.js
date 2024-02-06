6287660888b6cef85ce6d5a1d2261a4ef9a334bc
Vaishali <aggarwal.vaishali785@gmail.com>
2024-02-06 09:30:11 +0530

    docs: added a little function of recursion

--- Algorithms/Recursion.js
+++ Algorithms/Recursion.js
@@ -10,7 +10,7 @@ function inception() {
 inception();
 
 
-
+// *** Recursion is not an algorithm but its a concept, a very important concept.
 // 1. Identify the base case (or stop point). 
 // 2. Identify the recursive case.
 // 3. Get closer and closer and return when needed. Usually you have 2 returns.
