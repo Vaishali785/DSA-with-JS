327352deefcd1fabae7174325bbba29cc385c6a2
Vaishali <aggarwal.vaishali785@gmail.com>
2025-05-15 20:33:06 +0530

    added implementation of insertion sort

--- Algorithms/Sorting/InsertionSort.js
+++ Algorithms/Sorting/InsertionSort.js
@@ -1,7 +1,20 @@
-const nums = [8, 5, 2, 6, 9, 3, 1, 4, 0, 7]
+const nums1 = [8, 5, 2, 6, 9, 3, 1, 4, 0, 7]
 const nums2 = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
 
-function insertionSort(arr) {}
+const nums = [6, 7, 8, 1, 2, 0, 5]
+function insertionSort(arr) {
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] < arr[0]) {
+			arr.unshift(arr.splice(i, 1)[0])
+		} else {
+			for (let j = 1; j < i; j++) {
+				if (arr[i] < arr[j] && arr[i] > arr[j - 1]) {
+					arr.splice(j, 0, arr.splice(i, 1)[0])
+				}
+			}
+		}
+	}
+}
 
-insertionSort(nums)
-// console.log(nums)
+insertionSort(nums2)
+console.log(nums)
